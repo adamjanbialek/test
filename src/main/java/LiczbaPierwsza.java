c2ea1f383c9bb7e4512ba4fe6d2c280e406f7a6b
Adam Bialek <adamjanbialek@gmail.com>
2019-10-16 11:57:39 +0200

    czyLiczbaPierwsza

--- src/main/java/LiczbaPierwsza.java
+++ src/main/java/LiczbaPierwsza.java
@@ -1,7 +1,22 @@
-public class Person {
+import java.util.ArrayList;
+import java.util.List;
 
-    
-    public static void main(String[] args) {
+public class LiczbaPierwsza {
 
+    public static Boolean czyLiczbaPierwsza(int a){
+        List<Integer> dzielniki=new ArrayList<Integer>();
+        boolean czyPierwsza = false;
+        for(int i=1; i<=a; i++) {
+            if(a%i==0){
+                dzielniki.add(i);
+            }
+        }
+
+        if(dzielniki.size()<3 && dzielniki.contains(1) && dzielniki.contains(a)){
+            czyPierwsza=true;
+        }
+
+        return czyPierwsza;
     }
+
 }
--- src/test/java/LiczbaPierwszaTest.java
+++ src/test/java/LiczbaPierwszaTest.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+import org.junit.Assert;
 
 public class LiczbaPierwszaTest {
+    @Test
+    public void shouldCheckIfLiczbaPierwsza(){
+
+        Assert.assertTrue(LiczbaPierwsza.czyLiczbaPierwsza(7));
+        Assert.assertFalse(LiczbaPierwsza.czyLiczbaPierwsza(6));
+    }
 }
