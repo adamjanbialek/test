a8787dccfbdb8ea9ae4c061f49b854d681d15aec
Adam Bialek <adamjanbialek@gmail.com>
2019-10-16 11:55:49 +0200

    czyLiczbaPierwsza

--- src/main/java/LiczbaPierwsza.java
+++ src/main/java/LiczbaPierwsza.java
@@ -1,7 +1,7 @@
 public class Person {
-    public static void main(String[] args) {
-        System.out.println("Lalala");
 
-        System.out.println("Bababa");
+    
+    public static void main(String[] args) {
+
     }
 }
