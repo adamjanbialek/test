ce05e6f907662b595efbab59480be745c4cff497
Adam Bialek <adamjanbialek@gmail.com>
2019-10-16 12:51:07 +0200

    Dodane ABC

--- src/main/java/LiczbaPierwsza.java
+++ src/main/java/LiczbaPierwsza.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class LiczbaPierwsza {
 
@@ -19,4 +20,10 @@ public class LiczbaPierwsza {
         return czyPierwsza;
     }
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbę, żeby sprawdzić, czy to liczba pierwsza: ");
+        int sprawdzona = scanner.nextInt();
+        System.out.println(czyLiczbaPierwsza(sprawdzona));
+    }
 }
--- src/test/java/LiczbaPierwszaTest.java
+++ src/test/java/LiczbaPierwszaTest.java
@@ -5,7 +5,7 @@ public class LiczbaPierwszaTest {
     @Test
     public void shouldCheckIfLiczbaPierwsza(){
 
-        Assert.assertTrue(LiczbaPierwsza.czyLiczbaPierwsza(7));
-        Assert.assertFalse(LiczbaPierwsza.czyLiczbaPierwsza(6));
+        Assert.assertTrue(LiczbaPierwsza.czyLiczbaPierwsza(2));
+        Assert.assertTrue(LiczbaPierwsza.czyLiczbaPierwsza(1));
     }
 }
